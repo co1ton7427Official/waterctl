6e3c1f93be40b4cbc921bf2b7b7d9ee1ed1c2ba9
celesWuff <celeswuff@protonmail.com>
2024-11-17 19:05:22 +0800

    update error messages

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,5 @@
+import { isMobileDevice } from "./utils";
+
 interface ErrorCase {
   isHit: (msg: string) => boolean;
   output: (container: HTMLElement, error: unknown) => void;
@@ -16,7 +18,7 @@ const errorCases: ErrorCase[] = [
     showLogs: true,
   },
   {
-    isHit: (msg) => !!msg.match(/WATERCTL INTERNAL Refused/) || !!msg.match(/WATERCTL INTERNAL Bad key/),
+    isHit: (msg) => !!msg.match(/WATERCTL INTERNAL (?:Refused|Bad key)/),
     output: (container) => {
       container.innerText =
         "水控器拒绝启动。\n\n" +
@@ -38,9 +40,18 @@ const errorCases: ErrorCase[] = [
     showLogs: true,
   },
   {
-    isHit: (msg) => !!msg.match(/User denied the browser permission/),
+    isHit: (msg) => !!msg.match(/User denied the browser permission/) || (isMobileDevice() && !!msg.match(/NotFoundError|Bluetooth adapter not available/)),
     output: (container) => {
-      container.innerText = "蓝牙权限遭拒。\n\n请前往手机设置，授予浏览器“位置信息”权限。\n此权限不会用于定位，详情请参考源代码仓库内的";
+      container.innerText = "未授予蓝牙权限。\n\n请前往手机设置，授予浏览器“附近设备”权限。\n详情请参考源代码仓库内的";
+      container.innerHTML += "<a href='https://github.com/celesWuff/waterctl/blob/gh-pages/FAQ.md' target='_blank'>“疑难解答”</a>。";
+    },
+    isFatal: true,
+    showLogs: false,
+  },
+  {
+    isHit: (msg) => !isMobileDevice() && !!msg.match(/NotFoundError|Bluetooth adapter not available/),
+    output: (container) => {
+      container.innerText = "设备不支持蓝牙，或未授予蓝牙权限。\n\n详情请参考源代码仓库内的";
       container.innerHTML += "<a href='https://github.com/celesWuff/waterctl/blob/gh-pages/FAQ.md' target='_blank'>“疑难解答”</a>。";
     },
     isFatal: true,
@@ -50,15 +61,6 @@ const errorCases: ErrorCase[] = [
     isHit: (msg) => !!msg.match(/NetworkError/),
     output: (container) => {
       container.innerText = "连接不稳定，无法与水控器建立连接。\n请重试。";
-    },
-    isFatal: true,
-    showLogs: false,
-  },
-  {
-    isHit: (msg) => !navigator.bluetooth || !!msg.match(/Bluetooth adapter not available/) || !!msg.match(/NotFoundError/),
-    output: (container) => {
-      container.innerText = "不支持的浏览器。\n\n需要支持蓝牙的浏览器，如 Chrome 或 Edge。\n\n";
-      container.innerHTML += "<a href='https://github.com/celesWuff/waterctl/blob/gh-pages/FAQ.md' target='_blank'>查看支持列表</a>。";
     },
     isFatal: true,
     showLogs: false,
