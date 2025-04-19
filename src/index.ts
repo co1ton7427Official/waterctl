b032a6d46506fa95c597cdf2a610b81150f68bfc
celesWuff <celeswuff@protonmail.com>
2025-04-19 14:51:54 +0800

    ban csdn + gitcode

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import "./writeValueLogging";
 import { handleButtonClick } from "./bluetooth";
 import { registerServiceWorker, resizeWindow, setupInstallButton } from "./pwaHelper";
 import * as Sentry from "@sentry/browser";
+import { isCsdn } from "./utils";
 
 Sentry.init({
   dsn: "https://17d03841e2244d53abdbe587434efd5c@glitchtip.celeswuff.science/1",
@@ -14,6 +15,12 @@ Sentry.init({
 if (!navigator.bluetooth) {
   (document.querySelector(".supported") as HTMLElement).style.display = "none";
   (document.querySelector(".unsupported") as HTMLElement).style.display = "block";
+}
+
+if (isCsdn()) {
+  (document.querySelector(".supported") as HTMLElement).style.display = "none";
+  (document.querySelector(".unsupported") as HTMLElement).style.display = "none";
+  (document.querySelector(".csdn-warning") as HTMLElement).style.display = "block";
 }
 
 document.addEventListener("DOMContentLoaded", () => {
